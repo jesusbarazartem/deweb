1cd03206109514ce6da14c97e3ff49ee8b05b693
Jesus A. Barazarte <jesus@bicca.com.co>
2023-07-18 13:42:17 -0400

    Working template

---
+++ src/assets/js/main.js
@@ -0,0 +1,8 @@
+import 'bootstrap';
+import Typewriter from 'typewriter-effect/dist/core';
+
+new Typewriter('#typewriter', {
+  strings: ['Main office location', 'Winter Garden, FL'],
+  loop: true,
+  autoStart: true,
+});
